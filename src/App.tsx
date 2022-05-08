414a913f88a734b72f456f60b8f0086bf6e09243
jonahBerlyne <jonshaber@gmail.com>
2022-05-08 14:36:05 -0400

    added router config to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import HomePage from './Pages/HomePage';
+import LoginPage from './Pages/LoginPage';
+import RegisterPage from './Pages/RegisterPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AppRoute from './Routes/AppRoute';
+import AuthRoute from './Routes/AuthRoute';
 
 export default function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<AuthRoute><LoginPage/></AuthRoute>}/>
+        <Route path="/register" element={<AuthRoute><RegisterPage/></AuthRoute>}/>
+        <Route path="/" element={<AppRoute><HomePage/></AppRoute>}/>
+      </Routes>
+    </Router>
   );
 }
