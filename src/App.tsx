928554ad0e9a7d79de52c953b4cec346bbed16b2
jonahBerlyne <jonshaber@gmail.com>
2022-05-08 14:31:06 -0400

    added app route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomePage from './Pages/HomePage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
--- src/Routes/AppRoute.tsx
+++ src/Routes/AppRoute.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from "../firebaseConfig";
 
-function AppRoute() {
+export default function AppRoute ({children}: {children: any}) {
+ const [pending, setPending] = useState<boolean>(true);
+ const [currentUser, setCurrentUser] = useState<any>(null);
+
+ useEffect(() => {
+  const unsub = onAuthStateChanged(
+   auth,
+   user => {
+    user ? setCurrentUser(user) : setCurrentUser(null);
+    setPending(false);
+   },
+   err => {
+    alert(`Error: ${err}`);
+    setPending(false);
+   }
+  );
+
+  return unsub;
+ }, []);
+
+ if (pending) return null;
+
+ if (currentUser) {
   return (
-    <div>AppRoute</div>
-  )
+    <div>
+      {children}
+    </div>
+  );
+ } else {
+   return <Navigate to="/login" />;
+ }
 }
-
-export default AppRoute
