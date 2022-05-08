9cd1218f789597fccc6c831124a24ddafe5e4732
jonahBerlyne <jonshaber@gmail.com>
2022-05-08 18:06:39 -0400

    rendered register page

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import "../Styles/Auth.css";
-import {Form, FloatingLabel} from "react-bootstrap";
+import { Form, FloatingLabel } from "react-bootstrap";
 
 export default function LoginPage() {
 
@@ -11,12 +11,14 @@ export default function LoginPage() {
     <div className="auth">
       <img src="https://www.iconpacks.net/icons/2/free-twitter-logo-icon-2429-thumb.png" alt="Twitter Icon" />
       <p className="auth-header">Sign in to Twitter</p>
-      <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3 auth-input">
-        <Form.Control type="email" placeholder="name@example.com" value={email} onChange={e => setEmail(e.target.value)} required />
-      </FloatingLabel>
-      <FloatingLabel controlId="floatingPassword" label="Password" className="mb-3 auth-input">
-        <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required />
-      </FloatingLabel>
+      <>
+        <FloatingLabel controlId="floatingInput" label="Email" className="mb-3">
+          <Form.Control type="email" placeholder="Email" className="auth-input" value={email} onChange={e => setEmail(e.target.value)} required />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingInput" label="Password" className="mb-3">
+          <Form.Control type="password" placeholder="Password" className="auth-input" value={password} onChange={e => setPassword(e.target.value)} required />
+        </FloatingLabel>
+      </>
       <button className='btn btn-dark auth-btn' type="submit">Login</button>
     </div>
   );
--- src/Pages/RegisterPage.tsx
+++ src/Pages/RegisterPage.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "../Styles/Auth.css";
+import { Form, FloatingLabel } from "react-bootstrap";
 
 export default function RegisterPage() {
+
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+
   return (
-    <div>RegisterPage</div>
+    <div className="auth">
+      <img src="https://www.iconpacks.net/icons/2/free-twitter-logo-icon-2429-thumb.png" alt="Twitter Icon" />
+      <p className="auth-header">Sign in to Twitter</p>
+      <>
+        <FloatingLabel controlId="floatingInput" label="Name" className="mb-3">
+          <Form.Control type="text" placeholder="Name" className="auth-input" value={name} onChange={e => setName(e.target.value)} required />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingInput" label="Email" className="mb-3">
+          <Form.Control type="email" placeholder="Email" className="auth-input" value={email} onChange={e => setEmail(e.target.value)} required />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingInput" label="Password" className="mb-3">
+          <Form.Control type="password" placeholder="Password" className="auth-input" value={password} onChange={e => setPassword(e.target.value)} required />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingInput" label="Confirm Password" className="mb-3">
+          <Form.Control type="password" placeholder="Confirm Password" className="auth-input" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} required />
+        </FloatingLabel>
+      </>
+      <button className='btn btn-dark auth-btn' type="submit">Sign Up</button>
+    </div>
   );
 }
