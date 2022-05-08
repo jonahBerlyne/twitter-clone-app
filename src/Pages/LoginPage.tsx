0455f4eb3f7a7094a113d556375e3bb2e8f0ccd6
jonahBerlyne <jonshaber@gmail.com>
2022-05-08 15:46:03 -0400

    added floating labels to auth inputs

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import "../Styles/Auth.css";
-import Input from '../Components/Input';
+import {Form, FloatingLabel} from "react-bootstrap";
 
 export default function LoginPage() {
 
@@ -8,11 +8,16 @@ export default function LoginPage() {
   const [password, setPassword] = useState<string>("");
 
   return (
-    <div className="login">
-      <h1>Sign in to Twitter</h1>
-      <Input type="text" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
-      <Input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-      <button className='btn' type="submit">Login</button>
+    <div className="auth">
+      <img src="https://www.iconpacks.net/icons/2/free-twitter-logo-icon-2429-thumb.png" alt="Twitter Icon" />
+      <p className="auth-header">Sign in to Twitter</p>
+      <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3 auth-input">
+        <Form.Control type="email" placeholder="name@example.com" value={email} onChange={e => setEmail(e.target.value)} required />
+      </FloatingLabel>
+      <FloatingLabel controlId="floatingPassword" label="Password" className="mb-3 auth-input">
+        <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required />
+      </FloatingLabel>
+      <button className='btn btn-dark auth-btn' type="submit">Login</button>
     </div>
   );
 }
