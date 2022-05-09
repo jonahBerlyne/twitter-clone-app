fe3be810c6d41c866c15415a8d00734681043df9
jonahBerlyne <jonshaber@gmail.com>
2022-05-08 23:34:04 -0400

    added register fn

--- src/Components/Register/UserAndPhoto.tsx
+++ src/Components/Register/UserAndPhoto.tsx
@@ -10,9 +10,10 @@ interface UserAndPhotoForm {
  imgFile: any;
  imgFileErr: string | null;
  showSignUp: () => void;
+ register: () => Promise<any>;
 };
 
-export default function UserAndPhoto({ username, setUsername, choosePic, photoUrl, imgFile, imgFileErr, showSignUp }: UserAndPhotoForm) {
+export default function UserAndPhoto({ username, setUsername, choosePic, photoUrl, imgFile, imgFileErr, showSignUp, register }: UserAndPhotoForm) {
   return (
     <div className="auth">
       <img src="https://www.iconpacks.net/icons/2/free-twitter-logo-icon-2429-thumb.png" alt="Twitter Icon" />
@@ -22,21 +23,53 @@ export default function UserAndPhoto({ username, setUsername, choosePic, photoUr
 
         <div className="img-container">
           <h4>Add an image (optional):</h4>
-          <Form.Control type="file" className="auth-img-input" value={photoUrl} onChange={choosePic} />
+
+          <Avatar src={photoUrl} alt={photoUrl} />
+
+          <Form.Control 
+            type="file" 
+            className="auth-img-input" 
+            onChange={choosePic} 
+          />
+
           {imgFileErr && <h6>{imgFileErr}</h6>} 
-          {imgFile && <Avatar src={photoUrl} alt={photoUrl} />}
+
         </div>
 
         <div className="username-container">
           <p className="username-at">@</p>
-          <FloatingLabel controlId="floatingInput" label="username" className="mb-3">
-            <Form.Control type="text" placeholder="username" className="auth-input" value={username} onChange={e => setUsername(e.target.value)} required />
+
+          <FloatingLabel 
+            controlId="floatingInput" label="username" 
+            className="mb-3"
+          >
+
+            <Form.Control 
+              type="text" 
+              placeholder="username" className="auth-input" 
+              value={username} 
+              onChange={e => setUsername(e.target.value)} 
+              required 
+            />
+
           </FloatingLabel>
         </div>
 
       </>
-      
-      <button className='btn btn-dark auth-btn' type="button" onClick={() => showSignUp()}>Go back</button>
+
+      <button 
+        className='btn btn-dark auth-btn' type="button" 
+        onClick={() => showSignUp()}
+      >Go back
+      </button>
+
+      <button
+        className='btn btn-dark auth-btn'
+        type="submit"
+        onClick={register}
+      >Register
+      </button>
+
     </div>
   );
 }
--- src/Pages/RegisterPage.tsx
+++ src/Pages/RegisterPage.tsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import "../Styles/Auth.css";
 import SignUp from '../Components/Register/SignUp';
 import UserAndPhoto from '../Components/Register/UserAndPhoto';
+import fireDB, { auth } from '../firebaseConfig';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { doc, collection, setDoc } from 'firebase/firestore';
 
 export default function RegisterPage() {
 
@@ -35,6 +38,14 @@ export default function RegisterPage() {
   const [userAndPhotoIsShown, setUserAndPhotoIsShown] = useState<boolean>(false);
 
   const showUserAndPhoto = (): void => {
+    if (name === "" || email === "" || password === "" || confirmPassword === "") {
+      alert("Please fill in all fields before continuing.");
+      return;
+    }
+    if (password !== confirmPassword) {
+      alert("Please confirm your password before continuing.");
+      return;
+    }
     setSignUpIsShown(false);
     setUserAndPhotoIsShown(true);
   }
@@ -44,9 +55,34 @@ export default function RegisterPage() {
     setSignUpIsShown(true);
   }
 
+  const register = async (): Promise<any> => {
+    if (username === "") {
+      alert("Please type in a username");
+      return;
+    }
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      await updateProfile(userCredential.user, {
+        displayName: name,
+        photoURL: photoUrl
+      });
+      const docRef = doc(fireDB, "users", `${userCredential.user.uid}`);
+      const userDoc = {
+        name,
+        email,
+        password,
+        username,
+        photoUrl
+      };
+      await setDoc(docRef, userDoc);
+    } catch (err) {
+      alert(`Registration error: ${err}`);
+    }
+  }
+
   const signUpProps = { name, email, password, confirmPassword, setName, setEmail, setPassword, setConfirmPassword, showUserAndPhoto };
   
-  const userAndPhotoProps = { username, setUsername, choosePic, photoUrl, imgFile, imgFileErr, showSignUp };
+  const userAndPhotoProps = { username, setUsername, choosePic, photoUrl, imgFile, imgFileErr, showSignUp, register };
 
   return (
     <>
