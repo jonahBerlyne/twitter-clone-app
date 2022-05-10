09d172b0d46736984440a989347c61cb8bcde379
jonahBerlyne <jonshaber@gmail.com>
2022-05-10 15:18:27 -0400

    rendered trends section

--- src/Components/Trend.tsx
+++ src/Components/Trend.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import "../Styles/Trend.css";
+import { MoreHoriz } from "@mui/icons-material";
 
 interface TrendInterface {
  trendTopic: string;
@@ -6,26 +8,29 @@ interface TrendInterface {
  trending: string;
  trendBody?: string;
  trendFooter?: string;
+ trendingWith1?: string;
+ trendingWith2?: string;
  trendPhotoUrl?: string;
 };
 
-export default function Trend({ trendTopic, trendRelevance, trending, trendBody, trendFooter, trendPhotoUrl }: TrendInterface) {
+export default function Trend({ trendTopic, trendRelevance, trending, trendBody, trendFooter, trendingWith1, trendingWith2, trendPhotoUrl }: TrendInterface) {
   return (
     <div className="trend-container">
 
      <div className="trend-text">
       <div className="trend-header">
-       {trendTopic}
-       {trendRelevance}
+       <p>{trendTopic} · {trendRelevance} {!trendPhotoUrl && <MoreHoriz className="trend-dots" />}</p> 
       </div>
       <div className="trend">
        <p className="trending">{trending}</p>
        {trendBody && <p className="trend-body">{trendBody}</p>}
       </div>
       {trendFooter && <p className="trend-footer">{trendFooter}</p>}
+      {trendingWith1 && trendingWith2 && 
+      <p className="trending-with">Trending with <a className='trending-with-link' href="#">{trendingWith1}</a>, <a className='trending-with-link' href="#"> {trendingWith2}</a></p>}
      </div>
 
-     {trendPhotoUrl && <img src={trendPhotoUrl} alt={trending} />}
+     {trendPhotoUrl && <img src={trendPhotoUrl} alt={trending} height={100} width={100} />}
     </div>
   );
 }
--- src/Components/Widgets.tsx
+++ src/Components/Widgets.tsx
@@ -15,6 +15,39 @@ export default function Widgets() {
 
       <div className="trends-container">
         <p className="trends-header">What's happening</p>
+        <Trend 
+          trendTopic="Only on Twitter" trendRelevance="Trending" trending="#weekendvibe" 
+          trendBody='Join the conversation to get your morning started in the right way.'
+          trendFooter='25.8K Tweets'
+          trendPhotoUrl='/sunRising.jpeg' 
+        />
+        <Trend 
+          trendTopic="Sports"
+          trendRelevance="4 hours ago"
+          trending="Tom Brady"
+          trendFooter='1,452 Tweets'
+        />
+        <Trend 
+          trendTopic="Food"
+          trendRelevance='12 hours ago'
+          trending="National Pizza Day"
+          trendFooter="10.6K Tweets"
+        />
+        <Trend 
+          trendTopic="K-pop"
+          trendRelevance="Trending"
+          trending="BTS"
+          trendBody="BTS unveils the tracklist for the third CD of their upcoming anthology album Proof"
+          trendingWith1="STILL WITH You"
+          trendingWith2="#PROOF_TRACKLIST3"
+          trendPhotoUrl='/btsAlbum.jpeg'
+        />
+        <Trend 
+          trendTopic="Science"
+          trendRelevance='Trending'
+          trending="Andromeda Galaxy"
+          trendFooter="8,736 Tweets"
+        />
       </div>
 
     </div>
