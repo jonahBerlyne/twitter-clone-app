22748f3516b5b1a44c6778611fd35cb139626c77
jonahBerlyne <jonshaber@gmail.com>
2022-07-07 20:12:05 -0400

    added media query to widgets section

--- src/Components/Feed.tsx
+++ src/Components/Feed.tsx
@@ -66,13 +66,17 @@ export default function Feed({ name, photoUrl, username }: UserInfo) {
 
       <div className="feed-inputContainer">
         <div className="feed-input">
-          <Avatar style={{ height: "50px", width: "50px" }} src={photoUrl} alt={username} />
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-              <Form.Control as="textarea" data-testid="tweetBox" rows={3} cols={50} maxLength={140} size="lg" placeholder="What's happening?" value={tweet} onChange={e => setTweet(e.target.value)} className="feed-input-box" />
-            </Form.Group>
-          </Form>
-          <button data-testid="tweetBtn" type="submit" className="btn btn-primary tweet-btn" onClick={sendTweet} disabled={tweet === ""}>Tweet</button>
+          <div className="feed-input-left">
+            <Avatar style={{ height: "50px", width: "50px" }} src={photoUrl} alt={username} />
+            <Form>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Control as="textarea" data-testid="tweetBox" rows={3} cols={50} maxLength={140} size="lg" placeholder="What's happening?" value={tweet} onChange={e => setTweet(e.target.value)} className="feed-input-box" />
+              </Form.Group>
+            </Form>
+          </div>
+          <div className="feed-input-right">
+            <button data-testid="tweetBtn" type="submit" className="btn btn-primary tweet-btn" onClick={sendTweet} disabled={tweet === ""}>Tweet</button>
+          </div>
         </div>
         <div className="feed-border"></div>
       </div>
